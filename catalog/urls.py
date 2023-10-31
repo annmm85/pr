c1cd1a9c5e1d6da3c4ed8d2b8391e997ed44a7d7
AnnMm85 <8mona854@gmail.com>
2023-10-31 11:17:58 +0700

    newm

---
+++ catalog/urls.py
@@ -0,0 +1,8 @@
+from django.template.defaulttags import url
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^books/$', views.BookListView.as_view(), name='books'),
+]
